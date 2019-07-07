cac8bac5e79c33fb33c623489351cdd56a6259f4
gepetoOP <lucasrochaop@gmail.com>
2019-07-07 15:52:48 -0300

    agora vai de vdd mesmo versao final

--- src/misc/Configs.java
+++ src/misc/Configs.java
@@ -13,14 +13,16 @@ package misc;
 
 public class Configs {
 
-    public static final int QTD_SUBJECTS = 3;
+    public static final int QTD_SUBJECTS = 2;
+    public static final int QTD_OBSERVER = 6;
+    public static final int QTD_WRITER = 2;
 
     public static final int BATCH_SIZE = 50;
     public static final int TAMANHO_NUVEM_INICIAL = 250;
 
     public static final int TSE_PORTA = 9656;
     public static final int INVOKER_PORTA = 6789;
-    public static final int SUBJECT_PORTA = 4321;
+    public static final Integer SUBJECT_PORTA = 4321;
 //    public static final String SUBJECT_PORTA_STRING = "4321";
 
     public static final int TAMANHO_FRAME = Configs.TAMANHO_PANEL + 50;
